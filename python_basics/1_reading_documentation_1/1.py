8d39308581634fb6320746b59605e3910eb3f0b7
James Wallace <4538317+NZLWally@users.noreply.github.com>
2024-07-07 17:03:11 +1200

    Add exercise files

---
+++ python_basics/1_reading_documentation_1/1.py
@@ -0,0 +1,16 @@
+# What is the official place to find Python documentation?
+
+# docs.python.org
+
+
+# Solution
+
+# The Python Official documentation is a valuable resource that you should reference frequently as you learn Python. It provides both tutorials and a comprehensive library reference detailing Python's built-in modules, functions, classes, and more. Additionally, you can find information about Python's syntax, standard library, and development guides.
+
+# Before proceeding with this set of exercises, we recommend re-reading the Python Documentation section of our Introduction to Programming With Python book. Pay particular attention to the specific parts of the documentation that we mention.
+
+# The documentation exercises may prove a bit difficult and frustrating, but you should try to work them anyway. No matter which programming language or other technology you're using, you have to contend with the documentation. Sometimes, documentation is excellent; sometimes, it isn't. The Python documentation, unfortunately, belongs to the latter group.
+
+# Discussion
+
+# Take some time to familiarize yourself with the official Python documentation; you will need to reference it often. In particular, ensure you know where the search bar is, as it is often the fastest entry point - whether you're exploring list methods, examples of how the break statement works, or any other Python-specific topics. It's worth noting that you may sometimes have to visit several pages to completely understand a topic.
